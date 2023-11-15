d0b1bb181cb3d4e64efb80022b1f1d5f328cbc8d
volant97 <volant97@naver.com>
2023-11-15 09:58:15 +0900

    27

---
+++ 27.js
@@ -0,0 +1,23 @@
+// 핸드폰 번호 가리기
+
+function solution(phoneNumber) {
+  const frontNumber = phoneNumber.slice(0, phoneNumber.length - 4).replace(/[0-9]/g,"*");
+  const backNumber = phoneNumber.slice(phoneNumber.length - 4);
+ 
+  return frontNumber + backNumber;
+}
+
+console.log(solution("01033334444"));  // "*******4444"
+console.log(solution("027778888"));  // "*****8888"
+
+
+// 다른 풀이 1
+// function solution(s) {
+//   return s.replace(/\d(?=\d{4})/g, "*");
+// }
+
+// 다른 풀이 2
+// function solution(s){
+//   var result = "*".repeat(s.length - 4) + s.slice(-4);
+//   return result;
+// }
---
+++ 38.js
@@ -0,0 +1,12 @@
+// 직사각형 별찍기
+
+function solution(a, b) {
+  
+}
+
+console.log(solution(5, 3));
+// *****
+// *****
+// *****
+
+
