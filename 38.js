c415feb674101aa721d9eddfb38987ffe235281b
volant97 <volant97@naver.com>
2023-11-16 09:42:53 +0900

    38

--- 38.js
+++ 38.js
@@ -1,12 +1,31 @@
 // 직사각형 별찍기
 
-function solution(a, b) {
-  
-}
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
 
-console.log(solution(5, 3));
+  for (let i = 0; i < b; i++) {
+    let answer = ""
+    for (let j = 0; j < a; j++) {
+      answer += "*"
+    }
+    console.log(answer);
+  }
+});
+
+
+// 다른 풀이 1
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => {
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     const star = `${'*'.repeat(a)}\n`;
+
+//     console.log(star.repeat(b));
+// });
+
+// (5 3);
 // *****
 // *****
 // *****
-
-
