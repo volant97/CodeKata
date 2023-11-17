eb901c1787b662a1a810bafa4cd907bad8fc8463
volant97 <volant97@naver.com>
2023-11-17 09:52:14 +0900

    18

---
+++ 18.js
@@ -0,0 +1,21 @@
+// 문자열을 정수로 바꾸기
+
+function solution(s) {
+  let answer = Number(s);
+  return answer;
+}
+
+console.log(solution("1234"));  // 1234
+console.log(solution("-1234"));  // -1234
+
+
+// 다른 풀이 1
+// function strToInt(str) {
+//   return str / 1
+// }
+
+// 다른 풀이 2
+// unction strToInt(str){
+//   return +str;
+// }
+
