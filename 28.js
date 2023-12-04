e2314a840bbca05b42924d4c298cf1e3b05a4b38
volant97 <volant97@naver.com>
2023-12-05 00:02:38 +0900

    28

--- 28.js
+++ 28.js
@@ -8,6 +8,15 @@ function solution(numbers) {
   return answer;
 }
 
+// for문 줄이기
+// function solution(numbers) {
+//   let answer = 45
+//   for(let i in numbers){
+//       answer -= numbers[i]
+//   }
+//   return answer;
+// }
+
 // 다른 풀이 1
 // function solution(numbers) {
 //     return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
