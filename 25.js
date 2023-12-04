2dda5262f664c49eb3ede1acffc4b31ca27f74e1
volant97 <volant97@naver.com>
2023-12-04 09:33:53 +0900

    25

--- 25.js
+++ 25.js
@@ -2,9 +2,24 @@
 
 function solution(arr, divisor) {
   var answer = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor == 0) {
+      answer.push(arr[i]);
+    }
+  }
+  if (answer.length == 0) {
+    answer.push(-1);
+  }
+  answer.sort((a, b) => a - b);
   return answer;
 }
 
 console.log("solution : ", solution([5, 9, 7, 10], 5)); // 	[5, 10]
 console.log("solution : ", solution([2, 36, 1, 3], 1)); // 	[1, 2, 3, 36]
 console.log("solution : ", solution([3, 2, 6], 10)); // [-1]
+
+// 다른 풀이
+// function solution(arr, divisor) {
+//     var answer = arr.filter(v => v%divisor == 0);
+//     return answer.length == 0 ? [-1] : answer.sort((a,b) => a-b);
+// }
