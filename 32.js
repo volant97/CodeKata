f83839ab257f73a6d89f6f5a1a6affe53160ad83
volant97 <volant97@naver.com>
2023-12-07 09:52:50 +0900

    32

---
+++ 32.js
@@ -0,0 +1,19 @@
+// 내적
+
+function solution(a, b) {
+  let answer = 0;
+  for (i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4], [-3, -1, 0, 2])); // 	3
+
+// 다른 풀이 1 - reduce((acc, cur, idx)
+// function solution(a, b) {
+//     return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+// }
+
+// 인자에 _ 를 넣는 것은 사용하지 않는 인자일 경우 넣는 것으로 알고 있습니다.
+// 딱히 정해진 건 아니고 국룰 느낌~?
