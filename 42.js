65a2e80124abff202ae1e6d1cad66660c2231015
volant97 <volant97@naver.com>
2023-12-18 09:53:29 +0900

    42

---
+++ 42.js
@@ -0,0 +1,15 @@
+// 삼총사
+
+function solution(number) {
+  let answer = 0;
+  for (i = 0; i < number.length; i++) {
+    for (j = i + 1; j < number.length; j++) {
+      for (k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer += 1;
+        }
+      }
+    }
+  }
+  return answer;
+}
