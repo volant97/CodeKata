16ae68abcb213d2f8e6c31c1455fa5a9a9451405
volant97 <volant97@naver.com>
2024-03-25 23:38:28 +0900

    Lv1 14 다른풀이

--- 프로그래머스 Lv1/14_제일 작은 수 제거하기.js
+++ 프로그래머스 Lv1/14_제일 작은 수 제거하기.js
@@ -1,6 +1,21 @@
 function solution(arr) {
+  const min = Math.min(...arr);
+  const index = arr.indexOf(min);
+  arr.splice(index, 1);
+  if (arr.length === 0) arr.push(-1);
+  return arr;
+}
+
+// 다른풀이1
+function solution(arr) {
   const minNum = Math.min(...arr);
   const minNumIndex = arr.indexOf(minNum);
   arr.splice(minNumIndex, 1);
   return (answer = arr.length === 0 ? [-1] : arr);
+}
+
+// 다른풀이2
+function solution(arr) {
+  const min = Math.min(...arr);
+  return arr.length !== 1 ? arr.filter((i) => i !== min) : [-1];
 }
