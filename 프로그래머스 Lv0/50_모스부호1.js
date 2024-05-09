81defed5d6789e6f6ae5e924c051fb2cbd079174
volant97 <volant97@naver.com>
2024-05-10 02:47:59 +0900

    Lv0 50

---
+++ 프로그래머스 Lv0/50_모스부호1.js
@@ -0,0 +1,40 @@
+function solution(letter) {
+  morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+  return letter
+    .split(" ")
+    .map((i) => morse[i])
+    .join("");
+}
+
+// 다른풀이1
+// morse는 생략
+function solution(letter) {
+  return letter.split(" ").reduce((prev, curr) => prev + morse[curr], "");
+}
