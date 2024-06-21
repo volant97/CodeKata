7f9810993f557f525e21948a3b9595772b9c76d9
volant97 <volant97@naver.com>
2024-06-21 18:41:22 +0900

    Lv0 89.3 (풀이 완료)

--- 프로그래머스 Lv0/89.3_겹치는 선분의 길이.js
+++ 프로그래머스 Lv0/89.3_겹치는 선분의 길이.js
@@ -1,3 +1,49 @@
+// 89.3
+// 풀이 완료
+function solution(lines) {
+  const line = [
+    [...lines].sort((a, b) => a[0] - b[0])[0][0],
+    [...lines].sort((a, b) => b[1] - a[1])[0][1],
+  ];
+  const lineArr = new Array(line[1] - line[0] + 1)
+    .fill()
+    .map((v, i) => [line[0] + i, 0]);
+  let result = 0;
+
+  for (let i = lineArr[0][0]; i <= lineArr[lineArr.length - 1][0]; i++) {
+    if (i >= lines[0][0] && i < lines[0][1]) {
+      lineArr[i - line[0]][1]++;
+    }
+
+    if (i >= lines[1][0] && i < lines[1][1]) {
+      lineArr[i - line[0]][1]++;
+    }
+
+    if (i >= lines[2][0] && i < lines[2][1]) {
+      lineArr[i - line[0]][1]++;
+    }
+  }
+
+  lineArr.forEach((v) => {
+    if (v[1] >= 2) result++;
+  });
+
+  return result;
+}
+
+// 다른풀이1
+function solution(lines) {
+  let line = new Array(200).fill(0);
+
+  lines.forEach(([a, b]) => {
+    for (; a < b; a++) line[a + 100]++;
+  });
+
+  return line.reduce((a, c) => (c > 1 ? a + 1 : a), 0);
+}
+
+// -------------------------------------
+
 // 89.2
 // 3, 7번 테스트케이스 오류
 // 다시 풀기
