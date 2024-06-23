020d0be0100479eda6d9eb7c5f2d35245e882931
volant97 <volant97@naver.com>
2024-06-23 19:04:02 +0900

    Lv0 77.1

--- 프로그래머스 Lv0/77.1_로그인 성공.js
+++ 프로그래머스 Lv0/77.1_로그인 성공.js
@@ -1,3 +1,23 @@
+// 77.1
+function solution(id_pw, db) {
+  const id = id_pw[0];
+  const pw = id_pw[1];
+  let result = "fail";
+
+  for (let i = 0; i < db.length; i++) {
+    if (id === db[i][0]) {
+      if (pw === db[i][1]) {
+        result = "login";
+      } else {
+        result = "wrong pw";
+      }
+    }
+  }
+
+  return result;
+}
+
+// 기존 풀이
 function solution(id_pw, db) {
   let result = "fail";
 
