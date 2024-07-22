8e484cb8de56dc30ce6a89c52e15ff6120286309
volant97 <volant97@naver.com>
2024-07-22 15:51:26 +0900

    Lv0 119.1 다른풀이1~2

--- 프로그래머스 Lv0/119_문자열 정수의 합.js
+++ 프로그래머스 Lv0/119_문자열 정수의 합.js
@@ -3,3 +3,19 @@ function solution(num_str) {
   num_str.split("").forEach((i) => (result += Number(i)));
   return result;
 }
+
+// 다른풀이1
+// reduce
+function solution(num_str) {
+  return [...num_str].reduce((a, c) => a + +c, 0);
+}
+
+// 다른풀이2
+// reduce
+function solution(num_str) {
+  let result = 0;
+
+  result = num_str.split("").reduce((acc, cur) => acc + Number(cur), 0);
+
+  return result;
+}
