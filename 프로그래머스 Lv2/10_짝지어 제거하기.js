4d3a027e6e3ffefb390477a05108913a7cec621d
volant97 <volant97@naver.com>
2024-09-24 04:03:17 +0900

    Lv2 10

---
+++ 프로그래머스 Lv2/10_짝지어 제거하기.js
@@ -0,0 +1,29 @@
+function solution(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
+
+// 다른풀이1
+// for of
+function solution(s) {
+  const stack = [];
+
+  for (const char of s) {
+    if (stack.length > 0 && stack[stack.length - 1] === char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
