1689b54e3f3b90845223ff4a0a749dc6bff8ce34
volant97 <volant97@naver.com>
2024-10-31 17:54:38 +0900

    Lv0 189.1

--- 프로그래머스 Lv0/189_커피 심부름.js
+++ 프로그래머스 Lv0/189_커피 심부름.js
@@ -14,3 +14,16 @@ function solution(order) {
 // includes 조건의 latte만 추가하여 조건문 최소화
 const solution = (order) =>
   order.reduce((acc, cur) => acc + (cur.includes("latte") ? 5000 : 4500), 0);
+
+// 다른풀이2
+function solution(order) {
+  return order.reduce((acc, cur) => {
+    if (cur.includes("americano")) {
+      return acc + 4500;
+    } else if (cur.includes("cafelatte")) {
+      return acc + 5000;
+    } else {
+      return acc + 4500;
+    }
+  }, 0);
+}
